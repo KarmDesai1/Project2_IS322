1ef2815a7f8832bfb5dd5b09142253b9fc574e19
stephenjk41 <stephenjk41@gmail.com>
2020-04-25 21:54:09 -0400

    media query

--- proj2_final/src/Components/App.js
+++ proj2_final/src/Components/App.js
@@ -68,7 +68,21 @@ class App extends React.Component{
 
     render() {
         const{tab} =this.state;
-
+        <div>
+        <Media queries={{
+          small: "(max-width: 599px)",
+          medium: "(min-width: 600px) and (max-width: 1199px)",
+          large: "(min-width: 1200px)"
+        }}>
+          {matches => (
+            <Fragment>
+              {matches.small && <p>I am small!</p>}
+              {matches.medium && <p>I am medium!</p>}
+              {matches.large && <p>I am large!</p>}
+            </Fragment>
+          )}
+        </Media>
+      </div>
         switch(tab){
             case 'listView':
                 return(this.wrapPage(
